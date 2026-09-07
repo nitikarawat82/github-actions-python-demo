6b79612f709bdabbfcc85fdc83c57448c2fe3858
Nitika Rawat <93915470+nitikarawat82@users.noreply.github.com>
2026-09-07 15:57:19 +0530

    Update home route message in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is my Python App!"
+    return "Hello, my Python App was updated!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
